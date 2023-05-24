2fbd34553f3bf5acd85fb79a2d38e932743b612c
mrwrob <mrwrob@gmail.com>
2023-05-24 14:56:52 +0200

    Fix to load lables after youtube video reload

--- src/js/background.js
+++ src/js/background.js
@@ -1,12 +1,15 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("watch?v")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-    const urlParameterVideo = urlParameters.get("v");
-    
-    chrome.tabs.sendMessage(tabId, {
-      type: "init",
-      videoURL: urlParameterVideo,
-    });
-  }
+chrome.webNavigation.onCompleted.addListener((details) => {
+  console.log("HIST")
+  chrome.tabs.get(details.tabId, function(tab) {
+    if (tab.url && tab.url.includes("watch?v")) {
+      const queryParameters = tab.url.split("?")[1];
+      const urlParameters = new URLSearchParams(queryParameters);
+      const urlParameterVideo = urlParameters.get("v");
+      
+      chrome.tabs.sendMessage(details.tabId, {
+        type: "init",
+        videoURL: urlParameterVideo,
+      });
+    }
+  });
 });
